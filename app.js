225be8e1839e55a709485b9a330ead832bdd40e1
Josue Cano <jdevelopcano@gmail.com>
2021-07-09 18:18:33 -0500

    Empezada funcionalidad con js

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
+class Calculator{
+    constructor(previusDivText, currentDivText){
+        this.previusDivText = previusDivText;
+        this.currentDivText = currentDivText;
+        this.clear();
+    }
+
+    clear(){
+        this.currentText = "";
+        this.previusText = "";
+        this.operation = undefined;
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+        if(number === "." && this.currentText.includes(".")) return;
+        this.currentText = this.currentText.toString() + number.toString();
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+        this.currentDivText.innerText = this.currentText;
+    }
+}
 const numberBtns = document.querySelectorAll("[data-number]");
 const operationBtns = document.querySelectorAll("[data-operation]");
 const equalsBtn = document.querySelector("[data-equals]");
 const deleteBtn = document.querySelector("[data-delete]");
-const previusText = document.querySelector("[data-previus]");
-const currentText = document.querySelector("[data-current]");
+const previusDivText = document.querySelector("[data-previus]");
+const currentDivText = document.querySelector("[data-current]");
 
+const calculator = new Calculator(previusDivText, currentDivText);
 
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()});   
+});
