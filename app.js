c2e333d548217b8475342b8699496363db923c14
Josue Cano <jdevelopcano@gmail.com>
2021-07-08 23:57:00 -0500

    Add styles add app.js and const of buttons and text

---
+++ app.js
@@ -0,0 +1,8 @@
+const numberBtns = document.querySelectorAll("[data-number]");
+const operationBtns = document.querySelectorAll("[data-operation]");
+const equalsBtn = document.querySelector("[data-equals]");
+const deleteBtn = document.querySelector("[data-delete]");
+const previusText = document.querySelector("[data-previus]");
+const currentText = document.querySelector("[data-current]");
+
+
